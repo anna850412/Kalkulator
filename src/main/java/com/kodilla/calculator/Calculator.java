0d7f4fc060e90b9cf99aa6a7c1e4106398afe3fa
Anna Kołodziejczyk <anna.ak.kolodziejczyk@gmail.com>
2020-10-30 08:44:33 +0100

    actualization of calculator

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -1,15 +1,8 @@
 package com.kodilla.calculator;
 
 public class Calculator {
-    double a = 2;
-    double b = 5;
 
-    public Calculator() {
-        this.a = a;
-        this.b = b;
-    }
-
-    public double addAtoB(){
+    public double addAtoB(double a, double b){
         return a + b;
     }
     public double substractAfromB (double a , double b){
--- src/main/java/com/kodilla/calculator/CalculatorApplication.java
+++ src/main/java/com/kodilla/calculator/CalculatorApplication.java
@@ -1,6 +1,6 @@
 package com.kodilla.calculator;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +10,9 @@ public class CalculatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
         Calculator calculator = new Calculator();
-        calculator.addAtoB();
+        double result = calculator.addAtoB(5, 2);
         System.out.println("it is working");
+        System.out.println(result);
     }
 
 
